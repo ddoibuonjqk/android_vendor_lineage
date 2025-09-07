4645ea88e0d1cefe54bd79b91a442e0ff5f6d5eb
nhansp <nhan.094.vn@gmail.com>
2025-09-07 15:51:14 +0700

    Add MindTheGapps and Face Unlock from Evolution X
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -81,6 +81,20 @@ PRODUCT_PACKAGES += \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay
 
+# Face Unlock
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+ifneq ($(PRODUCT_NO_CAMERA),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+endif
+
 # Legal
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.lineagelegal.url=https://lineageos.org/legal
